fd5eebf0b2c7d54d3301beec248142074d7038c6
Carlo Colombo <carlo.colombo@gmx.com>
2020-12-10 21:53:13 +0100

    day 10 part 1

--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,13 +3,48 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
 
-    assert result
+  test "example 1" do
+    input = "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"
+    assert 220 = part1(input)
   end
+
+  test "part1" do
+    input = File.read!("./lib/advent_of_code/10_input.txt")
+
+    assert 23 == part1(input)
+  end
+
 
   @tag :skip
   test "part2" do
